c84c9cece1bdff229cfcb2980ade01efa7792369
amirali <amiralimoghadasi7@gmail.com>
2021-04-18 09:47:12 +0430

    RegisterAndLoginTest

--- src/controller/RegisterAndLoginController.java
+++ src/controller/RegisterAndLoginController.java
@@ -1,29 +1,28 @@
 package controller;
 
+import view.*;
+
 public class RegisterAndLoginController {
-        private void enterMenu(String menu)
-        {
-
-        }
 
 
-        public void findMatcher()
-        {
+    private void enterMenu(String menu) {
 
-        }
-
-
-        private void registerInGame(String username, String nickname, String password)
-        {
-
-        }
-
-
-        private void loginInGame(String username, String password)
-        {
-
-        }
     }
 
 
+    public void findMatcher() {
+        String command;
+        command=RegisterAndLoginView.getCommand();
 
+    }
+
+
+    private void registerInGame(String username, String nickname, String password) {
+
+    }
+
+
+    private void loginInGame(String username, String password) {
+
+    }
+}
--- src/view/RegisterAndLoginView.java
+++ src/view/RegisterAndLoginView.java
@@ -5,16 +5,16 @@ import model.*;
 
 import java.util.Scanner;
 
-class RegisterAndLoginView {
+public class RegisterAndLoginView {
 
 
-    public final String getCommand() {
+    public static String getCommand() {
         Scanner scanner = new Scanner(System.in);
         return scanner.nextLine();
     }
 
 
-    public final void showInput(String input) {
+    public static void showInput(String input) {
         System.out.println(input);
 
     }
