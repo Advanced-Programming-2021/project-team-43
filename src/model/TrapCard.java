f7724bc04635224ff6c8979ff8b38f53a66791bf
royadaneshi <dreamaid2020@hotmail.com>
2021-04-18 23:32:51 +0430

    make SpellCard class

--- src/model/TrapCard.java
+++ src/model/TrapCard.java
@@ -1,4 +1,51 @@
 package model;
 
+import java.util.HashMap;
+
 public class TrapCard {
+    private String cardName;
+    private String type;
+    private int cardNumber;
+    private String icon;
+    private String cardDescription;
+    private String cardOwner;
+    private static HashMap<String, TrapCard> trapCards = new HashMap<>();
+
+    public TrapCard(String cardName, String type, int cardNumber, String icon, String cardDescription,String cardOwner) {
+        this.cardName = cardName;
+        this.type = type;
+        this.cardNumber = cardNumber;
+        this.icon = icon;
+        this.cardDescription = cardDescription;
+        this.cardOwner=cardOwner;
+        trapCards.put(cardName, this);
+    }
+
+    public String getCardName() {
+        return cardName;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public int getCardNumber() {
+        return cardNumber;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public String getCardDescription() {
+        return cardDescription;
+    }
+
+    public String getCardOwner() {
+        return cardOwner;
+    }
+
+    public static TrapCard getTrapCardByName(String cardName) {
+        return trapCards.get(cardName);
+    }
 }
