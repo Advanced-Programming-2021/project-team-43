ddbed0234686c1e264561b91240ceda683013b22
amirali <amiralimoghadasi7@gmail.com>
2021-04-18 08:35:36 +0430

    hi

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,5 @@
-import controller.SetCards;
-import model.MonsterCard;
-import model.SpellCard;
-import model.TrapCard;
-
 public class Main {
-
     public static void main(String[] args) {
-        SetCards.readingCSVFileMonster();
-        SetCards.readingCSVFileTrapSpell();
 
     }
 }
