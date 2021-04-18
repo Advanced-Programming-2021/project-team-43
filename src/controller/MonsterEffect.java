2c6363057cb34c227b99d803dd5ca326edc67afe
royadaneshi <dreamaid2020@hotmail.com>
2021-04-19 00:08:50 +0430

    make MonsterEffect class

--- src/controller/MonsterEffect.java
+++ src/controller/MonsterEffect.java
@@ -1,4 +1,54 @@
 package controller;
 
 public class MonsterEffect {
+
+    public static  void commandKnight() {
+
+    }
+
+    public static  void yomiShip() {
+
+    }
+    public static  void suijin(){
+
+    }
+
+    public static  void manEaterBug(){
+
+    }
+    public static  void gateGuardian(){
+
+    }
+    public static  void scanner(){
+
+    }
+    public static  void marshmallon(){
+
+    }
+    public static  void beastKingBarbaros(){
+
+    }
+    public static void texchanger(){
+
+    }
+    public static void theCalculator(){
+
+    }
+    public static void mirageDragon(){
+
+    }
+    public static void heraldOfCreation(){
+
+    }
+    public static void exploderDragon(){
+
+    }
+    public static void terratigerTheEmpoweredWarrior(){
+
+    }
+    public static void theTricky(){
+
+    }
+
 }
+
