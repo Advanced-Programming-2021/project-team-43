32b8e0ff0569a0ca9f98a4771763f4c0cef8a008
amirali <amiralimoghadasi7@gmail.com>
2021-04-18 17:26:47 +0430

    sabt nam

---
+++ src/main/java/controller/MainMenu.java
@@ -0,0 +1,46 @@
+//package controller;
+//import view.*;
+//class MainMenuController
+//{
+//
+//    public static String username ;
+//
+//    public static void findMatcher(String username)
+//    {
+//        MainMenuController.username=username;
+//
+//
+//    }
+//
+//    private static void enterMenu(String menu)
+//    {
+//
+//    }
+//
+//
+//    public final String chooseFirstPlayer(String rivalName)
+//    {
+//
+//    }
+//
+//
+//
+//
+//
+//    private void logout()
+//    {
+//
+//    }
+//
+//
+//    private static void scoreboard()
+//    {
+//
+//    }
+//
+//
+//    private static void profile()
+//    {
+//
+//    }
+//}
