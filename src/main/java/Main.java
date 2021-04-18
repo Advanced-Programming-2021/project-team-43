0f52fa110d311a1edccfe44a90c4dc7256474906
royadaneshi <dreamaid2020@hotmail.com>
2021-04-18 10:08:47 +0430

    first

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,6 @@
-import main.java.controller.RegisterAndLoginController;
-
-
 public class Main {
+
     public static void main(String[] args) {
-        RegisterAndLoginController.findMatcher();
+        System.out.println("first");
     }
 }
