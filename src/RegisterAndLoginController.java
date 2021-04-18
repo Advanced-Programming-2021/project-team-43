071bdb9b3d477dc7c47e63176f628a5689603765
amirali <amiralimoghadasi7@gmail.com>
2021-04-18 09:10:26 +0430

    .

---
+++ src/RegisterAndLoginController.java
@@ -0,0 +1,33 @@
+
+public class RegisterAndLoginController {
+
+
+
+
+
+        private void enterMenu(String menu)
+        {
+
+        }
+
+
+        public void findMatcher()
+        {
+
+        }
+
+
+        private void registerInGame(String username, String nickname, String password)
+        {
+
+        }
+
+
+        private void loginInGame(String username, String password)
+        {
+
+        }
+    }
+
+
+
