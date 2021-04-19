dfea1ebec0ed40c560c54254d89bc4cb0dc2e360
royadaneshi <dreamaid2020@hotmail.com>
2021-04-19 23:02:00 +0430

    Some changes

--- src/controller/MonsterEffect.java
+++ src/controller/MonsterEffect.java
@@ -1,52 +1,79 @@
 package controller;
 
+import model.Card;
+import model.MonsterCard;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class MonsterEffect {
 
-    public static  void commandKnight() {
+    public static void commandKnight() {
+        MonsterCard.setEpic(true);
+        if (Card.getCardsByName("Command knight").getBackAndForth().equals("Forth")) {
+            HashMap<String, MonsterCard> monsters;
+            monsters = MonsterCard.getAllMonsters();
+            String[] keys;
+            keys= monsters.keySet().toArray(new String[0]);
+            for (String key : keys) {
+                MonsterCard.getMonsterByName(key).changeAttack(400);
+            }
+        }
+    }
+
+    public static void yomiShip(String nameOfAttacker) {
 
     }
 
-    public static  void yomiShip() {
-
-    }
-    public static  void suijin(){
+    public static void suijin() {
 
     }
 
-    public static  void manEaterBug(){
+    public static void manEaterBug() {
 
     }
-    public static  void gateGuardian(){
+
+    public static void gateGuardian() {
 
     }
-    public static  void scanner(){
+
+    public static void scanner() {
 
     }
-    public static  void marshmallon(){
+
+    public static void marshmallon() {
 
     }
-    public static  void beastKingBarbaros(){
+
+    public static void beastKingBarbaros() {
 
     }
-    public static void texchanger(){
+
+    public static void texchanger() {
 
     }
-    public static void theCalculator(){
+
+    public static void theCalculator() {
 
     }
-    public static void mirageDragon(){
+
+    public static void mirageDragon() {
 
     }
-    public static void heraldOfCreation(){
+
+    public static void heraldOfCreation() {
 
     }
-    public static void exploderDragon(){
+
+    public static void exploderDragon() {
 
     }
-    public static void terratigerTheEmpoweredWarrior(){
+
+    public static void terratigerTheEmpoweredWarrior() {
 
     }
-    public static void theTricky(){
+
+    public static void theTricky() {
 
     }
 
