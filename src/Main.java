6b2a42e3f8c1664d88fb06db8216371ac65ad350
amirali <amiralimoghadasi7@gmail.com>
2021-04-20 11:46:17 +0430

    start mainMenu

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
-import controller.RegisterAndLoginController;
+import main.java.controller.RegisterAndLoginController;
+
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
         RegisterAndLoginController.findMatcher();
+
     }
 }
---
+++ src/main/java/controller/DeckController.java
@@ -0,0 +1,5 @@
+package main.java.controller;
+
+public class DeckController {
+    public static void findMatcher(){}
+}
