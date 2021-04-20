9570f607e8dbf338b521f2dc3e4390c73c492e8d
Forooz99 <forooz.iraji@gmail.com>
2021-04-20 23:10:07 +0430

    add GameMat and Shop methods based on uml

---
+++ src/main/java/GameMatController.java
@@ -0,0 +1,118 @@
+import java.util.*;
+
+public class GameMatController {
+
+    public void selectMonsterPositionN(int address, boolean isOwnCard) {
+
+    }
+
+
+    public void selectDelete() {
+
+    }
+
+
+    public void changePhase(int phase) {
+
+    }
+
+
+    public void addCardToHand(String cardName) {
+
+    }
+
+
+    public void endPhase(String playerName) {
+
+    }
+
+
+    public void summon() {
+
+    }
+
+
+    public void setMonster() {
+
+    }
+
+
+    public void changeMonsterPosition(String mode) {
+
+    }
+
+
+    public void flipSummon() {
+
+    }
+
+
+    public void attack(int rivalCardAddress) {
+
+    }
+
+
+    public void attackDirect() {
+
+    }
+
+
+    public void activateSpellEffect() {
+
+    }
+
+
+    public void setSpell() {
+
+    }
+
+
+    public void setTrap() {
+
+    }
+
+
+    public void selfActivateEffectAndTrap() {
+
+    }
+
+
+    public void ritualSummon() {
+
+    }
+
+
+    public void specialSummon() {
+
+    }
+
+
+    public void showGraveyard() {
+
+    }
+
+
+    private void back() {
+
+    }
+
+
+    public void showOneCard(String cardName) {
+
+    }
+
+
+    public void surrender() {
+
+    }
+
+
+    private void enterMenu(String menu) {
+
+    }
+
+
+    public void findMatcher(String ownName, String rivalName) {
+
+    }
+}
---
+++ src/main/java/ShopController.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class ShopController {
+
+    private void shopShow() {
+
+    }
+
+
+    private void back() {
+
+    }
+
+
+    private void enterMenu(String menu) {
+
+    }
+
+
+    public void findMatcher() {
+
+    }
+
+
+    private void shopBuy(String cardName) {
+
+    }
+}
