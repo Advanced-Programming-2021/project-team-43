eaeb629c4209d05355f81d2651cd9d2758f8bed9
amirali <amiralimoghadasi7@gmail.com>
2021-04-20 20:45:22 +0430

    harchi emrooz zadam main mnue taghriban tamoome ueser model ham tamoome

--- src/main/java/model/DeckModel.java
+++ src/main/java/model/DeckModel.java
@@ -1,36 +1,72 @@
 package main.java.model;
 
+import main.java.controller.MainMenuController;
+import main.java.view.*;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 
 public class DeckModel {
     private String deckName;
-    private int cardNumber=0;
+    private int cardNumber = 0;
+    private int cardNumberSide=0;
+
+    public DeckModel(String deckName) {
+        this.deckName = deckName;
+    }
 
     public String getDeckName() {
         return deckName;
     }
 
 
-    private HashMap<String, Integer> cardNumberChecker;
+    public HashMap<String, Integer> cardsInMaindeck;
+    public HashMap<String, Integer> cardsInSidedeck;
     private ArrayList<String> cardName;
-    private boolean isMainDeck;
-    private boolean isSideDeck;
-
-
-
-    public void addCard(String cardName) {
-
+    public void addCardTomain(String cardName){
+        if(cardsInMaindeck.get(cardName)==null){
+            cardsInSidedeck.put(cardName,1);
+        }
+        else {
+            cardsInMaindeck.replace(cardName,cardsInMaindeck.get(cardName)+1);
+        }
+        cardNumber=cardNumber+1;
     }
 
 
-    public void removeCard(String cardName) {
+
+
+
+
+
+
+
+    public void removeCardToMain(String cardName) {
+        cardsInMaindeck.replace(cardName,cardsInMaindeck.get(cardName)-1);
+        if (cardsInMaindeck.get(cardName)==0){
+            cardsInMaindeck.remove(cardName);
+        }
+        cardNumber=cardNumber-1;
 
     }
 
+    public void addCardToSide(String cardName) {
+        if(cardsInSidedeck.get(cardName)==null){
+            cardsInSidedeck.put(cardName,1);
+        }
+        else {
+            cardsInSidedeck.replace(cardName,cardsInSidedeck.get(cardName)+1);
+        }
+        cardNumberSide=cardNumberSide+1;
 
-    public  void setMainDeck(String deckName) {
+    }
 
+    public void removeCardToSide(String cardName) {
+        cardsInSidedeck.replace(cardName,cardsInSidedeck.get(cardName)-1);
+        if (cardsInSidedeck.get(cardName)==0){
+            cardsInSidedeck.remove(cardName);
+        }
+        cardNumberSide=cardNumberSide-1;
     }
 
 
@@ -49,24 +85,19 @@ public class DeckModel {
 //    }
 
 
-    public  void setSideDeck(String sideDeck) {
+    public void setSideDeck(String sideDeck) {
 
     }
 
 
-//    public String getSideDeck() {
-//
-//    }
-
     public void setDeckName(String deckName) {
+
         this.deckName = deckName;
     }
 
-//    public int getCardNumber() {
-//        return cardNumber;
-//    }
-
-
-
 }
+
+
+
+
 
