08c0aec4e9b38810bd10ad29078c14db1e2754c7
Forooz99 <forooz.iraji@gmail.com>
2021-04-23 15:37:38 +0430

    Shop Model Almost Finished

--- src/main/java/ShopModel.java
+++ src/main/java/ShopModel.java
@@ -1,16 +1,18 @@
 import java.util.*;
 
+
 public class ShopModel {
-    private String cardName;
-    private int cardPrice;
     private static Map<String,Integer> cardInfo= new HashMap<>();
 
-    public int getCardPriceByName(String cardName) {
-        //return the card price by iterating over the cardInfo HashMap
+    public static int getCardPriceByName(String cardName) {
         for (Map.Entry<String,Integer> entry : cardInfo.entrySet())
             if (entry.getKey().equals(cardName))
                 return entry.getValue();
-        //return 0 if the cardName dont exist
         return 0;
+    }
+
+
+    public static HashMap<String,Integer> getCardInfo() {
+        return (HashMap<String, Integer>) cardInfo;
     }
 }
