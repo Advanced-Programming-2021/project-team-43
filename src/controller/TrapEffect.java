f9623592d87c2750474bf7f1af4f414708744139
royadaneshi <dreamaid2020@hotmail.com>
2021-04-24 23:25:37 +0430

    CSV

---
+++ src/controller/TrapEffect.java
@@ -0,0 +1,43 @@
+package controller;
+
+public class TrapEffect {
+
+    public static void magicCylinder() {
+
+    }
+    public static void mirrorForce() {
+
+    }
+
+    public static void mindCrush() {
+
+    }
+
+    public static void trapHole() {
+
+    }
+
+    public static void torrentialTribute() {
+
+    }
+
+    public static void timeSeal() {
+
+    }
+
+    public static void negateAttack() {
+
+    }
+    public static void solemnWarning() {
+
+    }
+    public static void magicJammer() {
+
+    }
+
+    public static void callOfTheHaunted() {
+
+    }
+
+
+}
