4d0a6c3be837f13d32c0986864ed697ae8b9a186
royadaneshi <dreamaid2020@hotmail.com>
2021-05-04 14:07:39 +0430

    update TrapEffect Class bacheha code in class ro comment kardam. chon bekhater estefadeh az tavabeaa namoojod error midad va push nemishod. baraye estefade az halat comment kharej konid.

--- src/main/java/controller/TrapEffect.java
+++ src/main/java/controller/TrapEffect.java
@@ -3,52 +3,54 @@ package main.java.controller;
 import main.java.model.Card;
 import main.java.model.MonsterZoneCard;
 import main.java.model.Player;
-import main.java.model.SpellZoneCard;
+import main.java.model.SpellTrapZoneCard;
+
 import java.util.HashMap;
+import java.util.Map;
 
 //public class TrapEffect {
-//    public static void trapEffectController(String nameOfTrapCard, String rival, String nameOfSummonCard,String player1,
+//    public static void trapEffectController(int addressOfTrapCard, String rival, int addressOfSummonCard, String player1,
 //                                            boolean summonMine) {
-//        if (nameOfTrapCard.equals("Mirror Force")) {
+//
+//        if (SpellTrapZoneCard.getSpellCardByAddress(addressOfTrapCard, player1).getSpellTrapName().equals("Mirror Force")) {
 //            mirrorForce(rival);
 //        }
-//        if (nameOfTrapCard.equals("Torrential Tribute")) {
-//            torrentialTribute(rival,player1);
+//        if (SpellTrapZoneCard.getSpellCardByAddress(addressOfTrapCard, player1).getSpellTrapName().equals("Torrential Tribute")) {
+//            torrentialTribute(rival, player1);
 //        }
-//        if (nameOfTrapCard.equals("Time Seal")) {
+//        if (SpellTrapZoneCard.getSpellCardByAddress(addressOfTrapCard, player1).getSpellTrapName().equals("Time Seal")) {
 //            timeSeal(rival);
 //        }
 //
-//        if (nameOfTrapCard.equals("Solemn Warning")) {
-//            solemnWarning(player1,nameOfSummonCard,summonMine,rival);
+//        if (SpellTrapZoneCard.getSpellCardByAddress(addressOfTrapCard, player1).getSpellTrapName().equals("Solemn Warning")) {
+//            solemnWarning(player1, addressOfSummonCard, summonMine, rival);
 //        }
-//        if (nameOfTrapCard.equals("Magic Jammer")) {
-//            magicJammer(player1,nameOfSummonCard,summonMine,rival);
+//        if (SpellTrapZoneCard.getSpellCardByAddress(addressOfTrapCard, player1).getSpellTrapName().equals("Magic Jammer")) {
+//            magicJammer(player1, addressOfSummonCard, summonMine, rival);
 //        }
 //    }
 //
-//    public static void mirrorForce(String rival) {
-//        HashMap<String, MonsterZoneCard> rivalsMonster = MonsterZoneCard.getMonsters(rival);
-//        String[] nameOfRivalsMonsters = rivalsMonster.keySet().toArray(new String[0]);
+//    public static void mirrorForce(String rival) {//kamel
+//        Map<Integer, MonsterZoneCard> rivalsMonster = MonsterZoneCard.getAllMonstersByPlayerName(rival);
+//        Integer[] addressOfRivalsMonsters = rivalsMonster.keySet().toArray(new Integer[0]);
 //        for (int i = 0; i < rivalsMonster.size(); i++) {
-//            if (MonsterZoneCard.getMonsterCardByName(nameOfRivalsMonsters[i], rival).getMode().equals("defend")) {
-//                MonsterZoneCard.deleteCard(nameOfRivalsMonsters[i], rival);
+//            if (MonsterZoneCard.getMonsterCardByAddress(addressOfRivalsMonsters[i], rival).getMode().equals("DO")) {
+//                MonsterZoneCard.getMonsterCardByAddress(addressOfRivalsMonsters[i], rival).removeMonsterFromZone();
 //            }
 //        }
 //    }
 //
 //
-//    public static void torrentialTribute(String rival, String player1) {
-//
-//        HashMap<String, MonsterZoneCard> rivalsMonster = MonsterZoneCard.getMonsters(rival);
-//        String[] monsterNames = rivalsMonster.keySet().toArray(new String[0]);
+//    public static void torrentialTribute(String rival, String player1) {//kamel
+//        Map<Integer, MonsterZoneCard> rivalsMonster = MonsterZoneCard.getAllMonstersByPlayerName(rival);
+//        Integer[] monsterNames = rivalsMonster.keySet().toArray(new Integer[0]);
 //        for (int i = 0; i < rivalsMonster.size(); i++) {
-//            MonsterZoneCard.deleteCard(monsterNames[i], rival);
+//            MonsterZoneCard.getMonsterCardByAddress(monsterNames[i], rival).removeMonsterFromZone();
 //        }
-//        HashMap<String, MonsterZoneCard> ownMonster = MonsterZoneCard.getMonsters(player1);
-//        String[] ownMonsterNames = ownMonster.keySet().toArray(new String[0]);
+//        Map<Integer, MonsterZoneCard> ownMonster = MonsterZoneCard.getAllMonstersByPlayerName(player1);
+//        Integer[] ownMonsterNames = ownMonster.keySet().toArray(new Integer[0]);
 //        for (int i = 0; i < ownMonster.size(); i++) {
-//            MonsterZoneCard.deleteCard(ownMonsterNames[i], player1);
+//            MonsterZoneCard.getMonsterCardByAddress(ownMonsterNames[i], player1).removeMonsterFromZone();
 //        }
 //    }
 //
@@ -57,34 +59,36 @@ import java.util.HashMap;
 //    }
 //
 //
-//    public static void solemnWarning(String player1, String nameOfSummonCard, boolean summonMine, String rival) {
+//    public static void solemnWarning(String player1, int addressOfSummonCard, boolean summonMine, String rival) {//kamel
 //        Player.getPlayerByName(player1).changeLifePoint(-2000);
-//        if (Card.getCardsByName(nameOfSummonCard).getCardModel().equals("Monster")) {
-//            if (summonMine) {
-//                MonsterZoneCard.deleteCard(nameOfSummonCard, player1);
+//        if (summonMine) {
+//            if (Card.getCardsByName(MonsterZoneCard.getMonsterCardByAddress(addressOfSummonCard, player1).getMonsterName()).getCardModel().equals("Monster")) {
+//                MonsterZoneCard.getMonsterCardByAddress(addressOfSummonCard, player1).removeMonsterFromZone();
 //            }
-//            MonsterZoneCard.deleteCard(nameOfSummonCard, rival);
+//            if (Card.getCardsByName(MonsterZoneCard.getMonsterCardByAddress(addressOfSummonCard, player1).getMonsterName()).getCardModel().equals("Spell") ||
+//                    Card.getCardsByName(MonsterZoneCard.getMonsterCardByAddress(addressOfSummonCard, player1).getMonsterName()).getCardModel().equals("Trap")) {
+//                SpellTrapZoneCard.getSpellCardByAddress(addressOfSummonCard, player1).removeSpellTrapFromZone();
+//            }
 //        }
-//        if (Card.getCardsByName(nameOfSummonCard).getCardModel().equals("Spell")) {
-//            if (summonMine) {
-//                SpellZoneCard.deleteCard(nameOfSummonCard, player1);
+//        if (!summonMine) {
+//            if (Card.getCardsByName(MonsterZoneCard.getMonsterCardByAddress(addressOfSummonCard, rival).getMonsterName()).getCardModel().equals("Monster")) {
+//                MonsterZoneCard.getMonsterCardByAddress(addressOfSummonCard, rival).removeMonsterFromZone();
 //            }
-//            SpellZoneCard.deleteCard(nameOfSummonCard, rival);
-//        }
-//        if (Card.getCardsByName(nameOfSummonCard).getCardModel().equals("Trap")) {
-//            if (summonMine) {
-//                TrapZoneCard.deleteCard(nameOfSummonCard, player1);
+//            if (Card.getCardsByName(MonsterZoneCard.getMonsterCardByAddress(addressOfSummonCard, rival).getMonsterName()).getCardModel().equals("Spell") ||
+//                    Card.getCardsByName(MonsterZoneCard.getMonsterCardByAddress(addressOfSummonCard, rival).getMonsterName()).getCardModel().equals("Trap")) {
+//                SpellTrapZoneCard.getSpellCardByAddress(addressOfSummonCard, rival).removeSpellTrapFromZone();
 //            }
-//            TrapZoneCard.deleteCard(nameOfSummonCard, rival);
 //        }
 //
 //    }
 //
-//    public static void magicJammer(String player1, String activateCardName, boolean activateMine, String rival) {
+//    public static void magicJammer(String player1, int activateCardAddress, boolean activateMine, String rival) {//kamel
 //        if (activateMine) {
-//            SpellZoneCard.deleteCard(activateCardName, player1);
+//            SpellTrapZoneCard.getSpellCardByAddress(activateCardAddress, player1).removeSpellTrapFromZone();
 //        }
-//        SpellZoneCard.deleteCard(activateCardName, rival);
+//        if (!activateMine) {
+//            SpellTrapZoneCard.getSpellCardByAddress(activateCardAddress, rival).removeSpellTrapFromZone();
+//        }
 //    }
 //
 //}
