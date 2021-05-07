d160259fa5c20fd7b830d88456a8328aaf6a1525
royadaneshi <dreamaid2020@hotmail.com>
2021-05-07 18:30:05 +0430

    JSON!

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,15 @@
 import main.java.controller.*;
 import main.java.model.*;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Main {
 
     public static void main(String[] args) {
-        RegisterAndLoginController.findMatcher();
+
         SetCards.readingCSVFileTrapSpell();
         SetCards.readingCSVFileMonster();
-
+     //   RegisterAndLoginController.findMatcher();
     }
 }
