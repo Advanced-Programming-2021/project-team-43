afef904b1ed04942a9213a5946ca418d021385b4
Forooz99 <forooz.iraji@gmail.com>
2021-05-13 10:06:10 +0430

    modify ShopModel

--- src/main/java/model/ShopModel.java
+++ src/main/java/model/ShopModel.java
@@ -6,12 +6,20 @@ public class ShopModel {
 
     private static Map<String,Integer> cardInfo= new HashMap<>();
 
+    public ShopModel() {
+        for (Map.Entry<String,Card> eachCard : Card.getCards().entrySet()) {
+            cardInfo.put(eachCard.getKey(), Card.getCardsByName(eachCard.getKey()).getPrice());
+        }
+    }
+
     public static int getCardPriceByName(String cardName) {
         for (Map.Entry<String,Integer> entry : cardInfo.entrySet())
             if (entry.getKey().equals(cardName))
                 return entry.getValue();
         return 0;
     }
+
+
 
     public static HashMap<String,Integer> getCardInfo() {
         return (HashMap<String, Integer>) cardInfo;
