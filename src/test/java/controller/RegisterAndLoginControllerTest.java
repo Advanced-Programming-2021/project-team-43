68af7c08e5a8c55f63241f1bd99178d3183805d4
amirali <amiralimoghadasi7@gmail.com>
2021-05-14 21:33:22 +0430

    RegisterAndLoginControllerTest

---
+++ src/test/java/controller/RegisterAndLoginControllerTest.java
@@ -0,0 +1,46 @@
+package controller;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+
+
+public class RegisterAndLoginControllerTest {
+
+    @Test
+    public void registerInGame() {
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+        RegisterAndLoginController.registerInGame("ali", "n", "p");
+        Assert.assertEquals("user created successfully!", outContent.toString().substring(0, outContent.toString().length() - 2));
+        outContent.reset();
+        RegisterAndLoginController.registerInGame("ali", "nn", "p");
+        Assert.assertEquals("user with username " + "ali" + " already exists", outContent.toString().substring(0, outContent.toString().length() - 2));
+
+        outContent.reset();
+        RegisterAndLoginController.registerInGame("aliii", "n", "p");
+        Assert.assertEquals("user with nickname " + "n" + " already exists", outContent.toString().substring(0, outContent.toString().length() - 2));
+    }
+
+    @Test
+    public void loginInGame() {
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+        outContent.reset();
+        RegisterAndLoginController.registerInGame("reza", "ff", "p");
+        RegisterAndLoginController.loginInGame("reza", "p");
+        Assert.assertEquals("user logged in successfully!", outContent.toString().substring(0, outContent.toString().length() - 2));
+
+        outContent.reset();
+        RegisterAndLoginController.loginInGame("ali", "pp");
+        Assert.assertEquals("Username and password didn’t match!", outContent.toString().substring(0, outContent.toString().length() - 2));
+
+        outContent.reset();
+        RegisterAndLoginController.loginInGame("alio", "p");
+        Assert.assertEquals("Username and password didn’t match!", outContent.toString().substring(0, outContent.toString().length() - 2));
+
+    }
+}
