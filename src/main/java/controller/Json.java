b4ebd9bdae73abd7e6afe1d2105ed97fdf2e0d31
royadaneshi <dreamaid2020@hotmail.com>
2021-07-09 09:23:44 +0430

    w

--- src/main/java/controller/Json.java
+++ src/main/java/controller/Json.java
@@ -11,6 +11,7 @@ import java.util.*;
 
 public class Json {
 
+
     public static void writeUserModelInfo(HashMap<String, UserModel> allUsersInfo, ArrayList<String> allUsernames, ArrayList<String> allUsersNicknames) {
         try {
             FileWriter writerInfo = new FileWriter("jsonUsersInfo.txt");
