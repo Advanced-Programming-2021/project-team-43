2fe7f7fb3aaed1dfe2a7b853a58aa05a3b08e8d0
royadaneshi <dreamaid2020@hotmail.com>
2021-07-09 09:19:31 +0430

    w

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,6 +5,7 @@ import controller.SetCards;
 public class Main {
 
     public static void main(String[] args) {
+
         SetCards.readingCSVFileTrapSpell();
         SetCards.readingCSVFileMonster();
         Run.run();
