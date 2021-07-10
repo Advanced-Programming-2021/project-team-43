3a0b9f16e223222cd0b815e435f0a43057e17ade
royadaneshi <dreamaid2020@hotmail.com>
2021-07-10 15:10:16 +0430

    PickFirstPlayer

--- src/main/java/controller/PickFirstPlayer.java
+++ src/main/java/controller/PickFirstPlayer.java
@@ -1,116 +1,97 @@
 package controller;
+
+import model.Player;
 import model.UserModel;
-import java.util.Random;
-import java.util.Scanner;
+import view.GameMatView;
+
+
+import java.io.IOException;
+import java.util.*;
+
 
 public class PickFirstPlayer {
 
-    public static String chose(String player1, String player2) {
-        if(player2.equals("AI"))
-            return player1;
-        if (player1.equals("AI"))
-            return player2;
-        Scanner scanner = new Scanner(System.in);
+    public static String result;
+
+    public static String rockPaperScissors(String player1, String player2, String firstChoice, String secondChoice) {
+        if (firstChoice.equals("rock")) {
+            if (secondChoice.equals("rock")) {
+
+                return  "@The game equalised";
+            }
+            if (secondChoice.equals("paper")) {
+                return player2+"@"+UserModel.getUserByUsername(player2).getNickname() + " is win";
+            }
+            if (secondChoice.equals("scissors")) {
+                return player1+"@"+UserModel.getUserByUsername(player1).getNickname() + " is win";
+            }
+        }
+        if (firstChoice.equals("paper")) {
+            if (secondChoice.equals("rock")) {
+                return player1+"@"+ UserModel.getUserByUsername(player1).getNickname() + " is win";
+            }
+            if (secondChoice.equals("paper")) {
+              return "@The game equalised";
+            }
+            if (secondChoice.equals("scissors")) {
+                return player2+"@"+ UserModel.getUserByUsername(player2).getNickname() + " is win";
+            }
+        }
+        if (firstChoice.equals("scissors")) {
+            if (secondChoice.equals("rock")) {
+                return player2+"@"+ UserModel.getUserByUsername(player2).getNickname() + " is win";
+            }
+            if (secondChoice.equals("paper")) {
+                return player1+"@"+UserModel.getUserByUsername(player1).getNickname() + " is win";
+            }
+            if (secondChoice.equals("scissors")) {
+              return "@The game equalised";
+            }
+        }
+        return null;
+    }
+
+    public static String chanceCoin(String player1, String player2) {
         Random rand = new Random();
         int n = rand.nextInt(2);
         if (n == 0) {
-            String winner = chanceCoin(player1, player2);
-            System.out.println(UserModel.getUserByUsername(winner).getNickname() + " do you want play first ? (yes or no)");
-            while (true) {
-                String string = scanner.nextLine().trim();
-                if (string.equals("yes"))
-                    return winner;
-                if (string.equals("no")) {
-                    if (winner.equals(player1))
-                        return player2;
-                    return player1;
-                }
-                System.out.println("invalid command");
-            }
-        }
-        String winner = rockPaperScissors(player1, player2);
-        System.out.println(UserModel.getUserByUsername(winner).getNickname() + " do you want play first ? (yes or no)");
-        while (true) {
-            String string = scanner.nextLine().trim();
-            if (string.equals("yes"))
-                return winner;
-            if (string.equals("no")) {
-                if (winner.equals(player1))
-                    return player2;
-                return player1;
-            }
-            System.out.println("invalid command");
-        }
-    }
-
-    private static String rockPaperScissors(String player1, String player2) {
-        Scanner scanner = new Scanner(System.in);
-        while (true) {
-            System.out.println(UserModel.getUserByUsername(player1).getNickname() + " is your turn");
-            System.out.println("rock or paper or scissors?");
-            String t1 = scanner.nextLine().trim();
-            while (!t1.equals("rock") && !t1.equals("paper") && !t1.equals("scissors")) {
-                System.out.println("invalid command");
-                System.out.println("rock or paper or scissors?");
-                t1 = scanner.nextLine().trim();
-            }
-            System.out.println(UserModel.getUserByUsername(player2).getNickname() + " is your turn");
-            System.out.println("rock or paper or scissors?");
-            String t2 = scanner.nextLine().trim();
-            while (!t2.equals("rock") && !t2.equals("paper") && !t2.equals("scissors")) {
-                System.out.println("invalid command");
-                System.out.println("rock or paper or scissors?");
-                t2 = scanner.nextLine().trim();
-            }
-            if (t1.equals("rock")) {
-                if (t2.equals("rock")) {
-                    System.out.println("The game equalised");
-                    continue;
-                }
-                if (t2.equals("paper")) {
-                    System.out.println(UserModel.getUserByUsername(player2).getNickname() + " is win");
-                    return player2;
-                }
-                if (t2.equals("scissors")) {
-                    System.out.println(UserModel.getUserByUsername(player1).getNickname() + " is win");
-                    return player1;
-                }
-            }
-            if (t1.equals("paper")) {
-                if (t2.equals("rock")) {
-                    System.out.println(UserModel.getUserByUsername(player1).getNickname() + " is win");
-                    return player1;
-                }
-                if (t2.equals("paper")) {
-                    System.out.println("The game equalised");
-
-                }
-                if (t2.equals("scissors")) {
-                    System.out.println(UserModel.getUserByUsername(player2).getNickname() + " is win");
-                    return player2;
-                }
-            }
-            if (t1.equals("scissors")) {
-                if (t2.equals("rock")) {
-                    System.out.println(UserModel.getUserByUsername(player2).getNickname() + " is win");
-                    return player2;
-                }
-                if (t2.equals("paper")) {
-                    System.out.println(UserModel.getUserByUsername(player1).getNickname() + " is win");
-                    return player1;
-                }
-                if (t2.equals("scissors"))
-                    System.out.println("The game equalised");
-            }
-        }
-    }
-
-    private static String chanceCoin(String player1, String player2) {
-        Random rand = new Random();
-        int n = rand.nextInt(2);
-        if (n == 0)
             return player1;
+        }
         return player2;
     }
 
+    public static String ply1 = MainMenuController.username;
+    public static String ply2 = MainMenuController.username2;
+
+    public static String chose(String username, String playerName) {
+        Random random = new Random();
+        int chance = random.nextInt(50);
+        if (ply1.equals("AI")) {
+            GameMatController.onlineUser = UserModel.getUserByUsername(ply2).getNickname();
+            GameMatController.rivalUser = UserModel.getUserByUsername(ply1).getNickname();
+            if (Player.getPlayerByName("AI") == null) {
+                new Player(GameMatController.onlineUser, UserModel.getUserByUsername(ply2).userAllDecks.get(UserModel.getUserByUsername(ply2).getActiveDeck()), true, MainMenuController.roundNumber1);
+                new Player(UserModel.getUserByUsername(ply1).getNickname(), UserModel.getUserByUsername(ply1).userAllDecks.get(UserModel.getUserByUsername(ply1).getActiveDeck()), false, MainMenuController.roundNumber1);
+
+            }
+            //Objects.requireNonNullElseGet(GameMatController.gameMatView, () -> GameMatController.gameMatView = new GameMatView()).start(primaryStage);
+        } else if (ply2.equals("AI")) {
+            GameMatController.onlineUser = UserModel.getUserByUsername(ply1).getNickname();
+            GameMatController.rivalUser = UserModel.getUserByUsername(ply2).getNickname();
+            if (Player.getPlayerByName("AI") == null) {
+                new Player(GameMatController.onlineUser, UserModel.getUserByUsername(ply1).userAllDecks.get(UserModel.getUserByUsername(ply1).getActiveDeck()), true, MainMenuController.roundNumber1);
+                new Player(UserModel.getUserByUsername(ply2).getNickname(), UserModel.getUserByUsername(ply2).userAllDecks.get(UserModel.getUserByUsername(ply2).getActiveDeck()), false, MainMenuController.roundNumber1);
+            }
+          //  Objects.requireNonNullElseGet(GameMatController.gameMatView, () -> GameMatController.gameMatView = new GameMatView()).start(primaryStage);
+        } else {
+            if (chance % 2 == 0) {
+                //   chanceCoin(ply1,ply2);
+                return "chanceCoin";
+            } else {
+                return "rockPaperScissors";
+//                rockPaperScissors(ply1,ply2,);
+            }
+        }
+return "AI";
+    }
 }
